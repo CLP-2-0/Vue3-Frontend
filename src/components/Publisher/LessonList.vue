b0f2fe0cd0f109de8b3bcaae25d7e4b27c0af6f8
quynhtdong <71563993+quynhtdong@users.noreply.github.com>
2022-11-12 17:32:20 -0600

    front - back connection; api testing

---
+++ src/components/Publisher/LessonList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        
+        
+    </div>
+</template>
+
+<script>
+import LessonApis from "@/apis/LessonApis"
+export default {
+    name:'Lesson List',
+    components:{
+    },
+    methods: {
+        async getOneLesson() {
+            const res = await LessonApis.getOneLesson()
+            console.log(res)
+        }
+    },
+    mounted() {
+    this.getOneLesson();
+  },
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Publisher/PublisherDashboard.vue
+++ src/components/Publisher/PublisherDashboard.vue
@@ -4,6 +4,7 @@
         <div class="container">
             <BookEditor />
         </div>
+        <LessonList />
         
     </div>
 </template>
@@ -11,11 +12,13 @@
 <script>
 import NavbarActive from '../NavbarActive.vue'
 import BookEditor from './BookEditor.vue'
+import LessonList from './LessonList.vue';
 export default {
     name:'Publisher Dashboard',
     components:{
         NavbarActive,
         BookEditor,
+        LessonList
     }
 }
 </script>
