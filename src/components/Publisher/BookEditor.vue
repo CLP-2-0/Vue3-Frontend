dc7f52f67441bb6ee57cf5cf97b87887bd4778d0
Vipul Lade <vipul.lade01@gmail.com>
2022-11-13 14:18:40 -0600

    vocab tab

--- src/components/Publisher/BookEditor.vue
+++ src/components/Publisher/BookEditor.vue
@@ -1,39 +1,134 @@
 <template>
-    <div>
-        <quill-editor v-model:content="content" contentType="html"
-                   ref="quillEditor"
-                   :options="editorOption" />
-        
-        <div class="content ql-editor" v-html="content"></div>
-        <!-- for binding content from quill-editor need v-model:content='' and contentType='html' -->
+  <div class="container">
+    <div class="form-group row">
+      <label for="title" class="col-sm-1 col-form-label">Title:</label>
+      <div class="col-sm-11">
+        <input
+          type="text"
+          class="form-control"
+          id="titleInput"
+          placeholder="Enter title here..."
+        />
+      </div>
     </div>
- 
+    <div class="content-block">
+      <label class="col-sm-1 col-form-label">Content:</label>
+      <quill-editor
+        v-model:content="content"
+        contentType="html"
+        ref="quillEditor"
+        :options="editorOption"
+      />
+      <div class="btn-container">
+        <button type="button" class="btn btn-outline-success" @click="save">
+          Save
+        </button>
+      </div>
+      <!-- for binding content from quill-editor need v-model:content='' and contentType='html' -->
+    </div>
+
+    <div class="preview">
+      <div class="title-container"><div id="title"></div></div>
+      <div id="content" class="content ql-editor"></div>
+    </div>
+  </div>
+  <Mock :red="red" />
 </template>
 
 <script>
-import { QuillEditor } from '@vueup/vue-quill'
-import '@vueup/vue-quill/dist/vue-quill.snow.css';
+import { QuillEditor } from "@vueup/vue-quill";
+import "@vueup/vue-quill/dist/vue-quill.snow.css";
+import Mock from "./Mock.vue";
+
 export default {
-    name:'quil-editor',
-    title:'Book Editor',
-    components: {
-    QuillEditor
-    },
-    data() {
+  name: "quil-editor",
+  title: "Book Editor",
+  components: {
+    QuillEditor,
+    Mock,
+  },
+  data() {
     return {
       editorOption: {
         theme: "snow",
         placeholder: "",
         modules: {
-          toolbar: ["bold", "italic", "underline", "link", "clean"]
-        }
+          toolbar: [
+            "bold",
+            "italic",
+            "underline",
+            "link",
+            "clean",
+            { color: [] },
+            { background: [] },
+          ],
+        },
       },
-      content:'',
+      content: "",
+      title: "",
+      red: [
+        { word: "vipul", pinyin: "", grammar: "", meaning: "" },
+        { word: "chase", pinyin: "", grammer: "", meaning: "" },
+        { word: "bao", pinyin: "", grammar: "", meaning: "" },
+      ],
+      yellow: [],
+      // pinyin: [],
+      // type: [],
     };
-  }
-}
+  },
+  methods: {
+    save() {
+      this.title = document.getElementById("titleInput").value;
+      document.getElementById("title").innerHTML = this.title;
+
+      let contentElement = document.getElementById("content");
+      contentElement.innerHTML = this.content;
+      let children = contentElement.children;
+      for (let i = 0; i < children.length; i++) {
+        let child = children[i];
+        let spanChildren = child.children;
+        for (let j = 0; j < spanChildren.length; j++) {
+          let spanChild = spanChildren[j];
+
+          if (spanChild.getAttribute("style") == "color: red;") {
+            // this.red.word = spanChild.innerHTML;
+            this.red.push({
+              word: spanChild.innerHTML,
+              pinyin: "",
+              grammar: "",
+              meaning: "",
+            });
+          } else if (
+            spanChild.getAttribute("style") == "color: rgb(237, 125, 49);"
+          ) {
+            this.yellow.push(spanChild.innerHTML);
+          }
+        }
+      }
+
+      console.log(this.red);
+      console.log(this.yellow);
+    },
+  },
+};
 </script>
 
 <style>
+.container > div {
+  margin-top: 10px;
+}
 
+.btn-container {
+  display: flex;
+  justify-content: end;
+}
+
+.btn {
+  margin-top: 10px;
+}
+
+.title-container {
+  display: flex;
+  justify-content: center;
+}
 </style>
---
+++ src/components/Publisher/Mock.vue
@@ -0,0 +1,67 @@
+<template>
+  <table class="table" id="vocabTable">
+    <thead>
+      <tr>
+        <th scope="col">汉字</th>
+        <th scope="col">汉语拼音</th>
+        <th scope="col">词性</th>
+        <th scope="col">英文意思</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(r, index) in red">
+        <th scope="row">{{ r.word }}</th>
+        <td>
+          <textarea
+            name="pinyin"
+            type="text"
+            cols="10"
+            rows="1"
+            :pinyin="red.pinyin"
+            @blur="addToRed()"
+          ></textarea>
+        </td>
+        <td>
+          <textarea
+            name="grammar"
+            id="type"
+            cols="10"
+            rows="1"
+            @input="addToRed()"
+          ></textarea>
+        </td>
+        <td>
+          <textarea
+            name="meaning"
+            id="meaning"
+            cols="40"
+            rows="1"
+            @input="addToRed()"
+          ></textarea>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: ["red"],
+  emits: ["red"],
+  pinyinnew: pinyin,
+
+  //   emits: ["update:type", 'update:pinyin'],
+  name: "Mock",
+  components: {},
+
+  methods: {
+    addToRed() {
+      this.red.pinyin = pinyinnew;
+      this.red.grammer = document.getElementsByName("grammar");
+      this.red.meaning = document.getElementsByName("meaning");
+      console.log(this.red);
+      //   this.red.pinyin = document.getElementsByName("pinyin");
+    },
+  },
+};
+</script>
