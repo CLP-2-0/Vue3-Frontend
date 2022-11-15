4efcd8c0ac24e1b1911f6d27ce8eb26ac54beca0
dacinve <78516378+dacinve@users.noreply.github.com>
2022-11-14 19:30:58 -0500

    CRUD Lesson front-end to back-end complete

--- src/components/Publisher/LessonList.vue
+++ src/components/Publisher/LessonList.vue
@@ -1,6 +1,43 @@
 <template>
     <div>
-        
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" @click="addClick()">New Lesson</button>
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog ">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">{{modalTitle}}</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                        <div class="mb-3">
+                            <label for="#" class="form-label center-label">Lesson #</label>
+                            <input type="text" class="form-control" v-model="newLessons.LessonId" v-if="flagEdit==0" >
+                            
+                            <input type="text" class="form-control" v-model="newLessons.LessonId" v-if="flagEdit==1" disabled>
+                            
+                            
+
+                        </div>
+                        <div class="mb-3">
+                            <label for="#" class="form-label">Tilte</label>
+                            <input type="text" class="form-control" v-model="newLessons.LessonTitle">
+                            
+                        </div>
+                        
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary text-center" data-bs-dismiss="modal" v-if="flagEdit==0" @click="createLesson()" >Create</button>
+                        <button type="submit" class="btn btn-primary text-center" data-bs-dismiss="modal" v-if="flagEdit==1" @click="updateLesson()" >Update</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
         <div style="margin-top:30px">
             <table class="table table-hover">
                 <!-- <thead>
@@ -18,8 +55,8 @@
                         
                     </td>
                     <td class="text-right"><div >
-                        <button class="btn btn-light mx-3 ">Edit</button>
-                        <button class="btn btn-light">Delete</button>
+                        <button class="btn btn-light mx-3 " data-bs-toggle="modal" data-bs-target="#exampleModal" @click="editClick(lesson)">Edit</button>
+                        <button class="btn btn-light" @click="deleteLesson(lesson.id)">Delete</button>
                     </div>
                     </td>
                     
@@ -28,20 +65,26 @@
                 </tbody>
             </table>
             </div>
-            <!-- <p>{{lessons}}</p> -->
-        
     </div>
 </template>
 
 <script>
 import LessonApis from "@/apis/LessonApis"
+import axios from "axios";
 export default {
     name:'Lesson List',
     components:{
     },
     data(){
         return{ 
-            lessons:[]
+            lessons:[],
+            modalTitle: '',
+            newLessons:{
+                LessonTitle:'',
+                LessonId:0, 
+            },
+            flagEdit:0, 
+            
         }
     },
     methods: {
@@ -50,6 +93,60 @@ export default {
             this.lessons = res.data
             // console.log("this from list",res.data)
             // console.log("lessons res:", this.lessons)
+        },
+        async addClick() {
+            // console.log("this is click")
+            this.modalTitle = "Add lesson"
+            this.newLessons.LessonId = '';
+            this.newLessons.LessonTitle ='';
+            
+        },
+        async editClick(lesson) {
+            this.flagEdit = 1
+            // console.log(lesson.id)
+            // console.log(lesson.title)
+            this.modalTitle = "Edit lesson"
+            this.newLessons.LessonTitle = lesson.title
+            this.newLessons.LessonId = lesson.id
+        },
+        async createLesson(){
+            await axios.post("http://localhost:8000/lessons/",
+            {   id:this.newLessons.LessonId,
+                title:this.newLessons.LessonTitle
+            })
+            .then((res) =>{
+                
+                console.log(res.data);
+                console.log(this.newLessons);
+                // alert("Created Successfully")
+                this.getLessons();
+                this.$router.go(this.$router.currentRoute)
+            })
+        },
+        async updateLesson(){
+            await axios.put("http://localhost:8000/lessons/"+this.newLessons.LessonId,
+            {   id:this.newLessons.LessonId,
+                title:this.newLessons.LessonTitle
+            })
+            .then((res) =>{
+                console.log(res.data);
+                console.log(this.newLessons);
+                this.getLessons();
+                // alert("Updated Successfully")
+                this.$router.go(this.$router.currentRoute)
+                
+            })
+        },
+        async deleteLesson(id){
+            if(!confirm("Are you sure you want to delete")){
+                return;
+            }
+            await axios.delete("http://localhost:8000/lessons/"+id)
+            .then ((res) =>{
+                this.getLessons();
+                alert("Deleted Successfully"); 
+            })
+
         }
     },
     mounted() {
--- src/components/Publisher/PublisherDashboard.vue
+++ src/components/Publisher/PublisherDashboard.vue
@@ -1,40 +1,7 @@
 <template>
     <div>
         <NavbarActive />
-        <div class="container">
-            
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">ADD+</button>
-            <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog ">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Add lesson</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <form>
-                        <div class="mb-3">
-                            <label for="#" class="form-label center-label">Lesson #</label>
-                            <input type="text" class="form-control" id="lessonNumber" aria-describedby="emailHelp">
-
-                        </div>
-                        <div class="mb-3">
-                            <label for="#" class="form-label">Tilte</label>
-                            <input type="text" class="form-control" id="lessonTitle">
-                        </div>
-                        
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="submit" class="btn btn-primary text-center">Create</button>
-                        <!-- <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Close</button> -->
-                    </div>
-                </div>
-            </div>
-            </div>
-
+        <div class="container">            
             <LessonList />
             <!-- <BookEditor /> -->
         </div>
