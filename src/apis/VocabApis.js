07c302eed0719807471e9cce0be573a40b5ccac2
Vipul Lade <vipul.lade01@gmail.com>
2022-11-16 11:12:49 -0600

    working

--- src/apis/VocabApis.js
+++ src/apis/VocabApis.js
@@ -1,19 +1,8 @@
 import axios from "axios";
 
-const save = async () => {
-    const instance = axios.create({
-      baseURL: "http://localhost:8000",
-      timeout: 1000,
-    });
-    return await instance
-      .get("/vocab/1")
-      .then((res) => {
-        console.log(res.data)
-        return res.data;
-      })
-      .catch((error) => {
-        console.error(error.response.data);
-      });
-  }
+const instance = axios.create({
+  baseURL: "http://localhost:8000",
+  timeout: 1000,
+});
 
-  export default {save}
+const saveVocabList = async()
--- src/components/Publisher/BookEditor.vue
+++ src/components/Publisher/BookEditor.vue
@@ -78,6 +78,7 @@ export default {
   },
   methods: {
     save() {
+      red.length = 0;
       this.title = document.getElementById("titleInput").value;
       document.getElementById("title").innerHTML = this.title;
 
