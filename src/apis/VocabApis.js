dad2a92468baed273797fc87c98ae2d1776320ac
Vipul Lade <vipul.lade01@gmail.com>
2022-11-24 16:56:19 -0600

    not imp

--- src/apis/VocabApis.js
+++ src/apis/VocabApis.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
 const save = async () => {
-    const instance = axios.create({
-      baseURL: "http://localhost:8000",
-      timeout: 1000,
+  const instance = axios.create({
+    baseURL: "http://localhost:8000",
+    timeout: 1000,
+  });
+  return await instance
+    .get("/vocab/1")
+    .then((res) => {
+      console.log(res.data);
+      return res.data;
+    })
+    .catch((error) => {
+      console.error(error.response.data);
     });
-    return await instance
-      .get("/vocab/1")
-      .then((res) => {
-        console.log(res.data)
-        return res.data;
-      })
-      .catch((error) => {
-        console.error(error.response.data);
-      });
-  }
+};
 
-  export default {save}
+export default { save };
---
+++ src/components/Translate.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="btn-container">
+    <button
+      type="button"
+      class="btn btn-outline-success"
+      @click="chineseToPinyin"
+    >
+      Save
+    </button>
+  </div>
+</template>
+<script>
+// import { pinyin } from "@napi-rs/pinyin";
+export default {
+  props: [],
+  text: "中心",
+  data() {
+    return;
+  },
+  methods: {
+    // chineseToPinyin(text) {
+    //   console.log(pinyin(text));
+    // },
+  },
+};
+</script>
