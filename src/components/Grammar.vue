5efc737dec960c288e658ef8484b51ade7a0a2db
Vipul Lade <vipul.lade01@gmail.com>
2023-02-22 15:29:03 -0600

    lesson from main

---
+++ src/components/Grammar.vue
@@ -0,0 +1,26 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th class="col-6"></th>
+        <th></th>
+      </tr>
+    </thead>
+  </table>
+</template>
+
+<script>
+// import LessonApis from "@/apis/LessonApis.js";
+
+// export default {
+//   props: ["lessonIdx"],
+//   data() {
+//     return {
+//       Grammar: [],
+//     };
+//   },
+//   methods: {
+//     async,
+//   },
+// };
+</script>
